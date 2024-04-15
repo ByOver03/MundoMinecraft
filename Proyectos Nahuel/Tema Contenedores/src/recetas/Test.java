cc13f81dca6486c262b1de2c56cdf1318a13278f
Nahuel_Ramis <nrobert19684@iesjoanramis.org>
2024-04-15 15:34:54 +0200

    ClaseTest,Cambios

--- Proyectos Nahuel/Tema Contenedores/src/recetas/Test.java
+++ Proyectos Nahuel/Tema Contenedores/src/recetas/Test.java
@@ -1,6 +1,9 @@
 package recetas;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Scanner;
+import java.util.List;
 public class Test {
         public static void main(String[] args) {
             String bienvenida="Bienvenido a este programa donde podras:\n-Crear tus propias recetas\n-Introducir nuevos ingredientes\n-Ver los ingredientes y las recetas creadas hasta el momento";
@@ -24,10 +27,10 @@ public class Test {
     
             switch (introduccionUsuario) {
                 case 1:
-                    mostrarIngredientes(leer);
+                    mostrarIngredientes(leer, ingredientes);
                     break;
                 case 2:
-                    mostrarRecetas(leer);
+                    mostrarRecetas(leer, recetas);
                     break;
                 case 3:
                     crearIngrediente(ingredientes, leer);
@@ -38,9 +41,9 @@ public class Test {
                 case 5:
                     System.out.println("Aqui tienes los ingredentes y las recetas que has registrado");
                     System.out.println("Ingredientes:");
-                    mostrarIngredientes(leer);
+                    mostrarIngredientes(leer, ingredientes);
                     System.out.println("Recetas:");
-                    mostrarRecetas(leer);
+                    mostrarRecetas(leer, recetas);
             }
         }
     
@@ -77,19 +80,50 @@ public class Test {
         }
     
         //Metodo para listar los ingredientes registrados
-        static void  mostrarIngredientes(Scanner leer){
+        static void  mostrarIngredientes(Scanner leer, HashSet<Ingrediente> ingredientes){
             System.out.println("Como desea que le aparezcan los ingredientes?");
             System.out.println("[1] Ordenado alfabeticamente\n" +
             "[2] Ordenado por el precio\n" + 
             "[3] Volver atras");
+            int respuesta = leer.nextInt();
+            ArrayList<Ingrediente> lingrediente= new ArrayList<>(ingredientes);
+            OrdenaIngredientesNombre m =new OrdenaIngredientesNombre();
+            OrdenaIngredientesPrecio n= new OrdenaIngredientesPrecio();
+            switch (respuesta) {
+                case 1:
+                    Collections.sort((List)lingrediente, m);
+                    break;
+                case 2:
+                    Collections.sort((List)lingrediente, n);
+                    break;
+                case 3:
+                    menuPrincipal(leer, null, ingredientes);
+                    break;
+            }
         }
     
         //Metodo para listar las recetas registradas
-        static void mostrarRecetas(Scanner leer){
+        static void mostrarRecetas(Scanner leer, HashSet<Receta> recetas){
             System.out.println("Como desea que le aparezcan las recetas?");
             System.out.println("[1] Ordenado alfabeticamente\n" +
             "[2] Ordenado por el precio\n" + 
             "[3] Volver atras");
+            OrdenaRecetaNombre m = new OrdenaRecetaNombre();
+            OrdenaRecetaPrecio n= new OrdenaRecetaPrecio();
+            int respuesta= leer.nextInt();
+            ArrayList<Receta> lReceta= new ArrayList<>(recetas);
+            
+            switch (respuesta) {
+                case 1:
+                    Collections.sort((List)lReceta, m);
+                    break;
+                case 2:
+                    Collections.sort((List)lReceta, n);
+                    break;
+                case 3:
+                    menuPrincipal(leer, recetas, null);
+                    break;
+            }
         }
     }
     
