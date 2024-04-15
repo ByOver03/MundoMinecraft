e14410c087b92bdfb3fb235c4588f7cf34e0f4ef
ByOver03 <nahuelrobert1718@gmail.com>
2024-04-16 00:01:11 +0200

    Frutas Terminado

--- Proyectos Nahuel/Tema Contenedores/src/fruta/Fruit.java
+++ Proyectos Nahuel/Tema Contenedores/src/fruta/Fruit.java
@@ -1,7 +1,38 @@
 package fruta;
 
 public class Fruit {
+    
+    //Atributos
     String nombre;
-    double precio;
+    private double precio;
+
+    //getters
+    public double getPrecio() {
+        return precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    //setters
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    //Constructor
+    Fruit(String nombre, double precio){
+        this.nombre= nombre;
+        setPrecio(precio);
+    }
+
+    public String toString(){
+        return "Nombre: " + getNombre() + " Precio: " + getPrecio();
+    }
     
 }
