d9994a8da5e1a1fc071052818d0934c14ddb13d8
Nahuel_Ramis <nrobert19684@iesjoanramis.org>
2024-04-16 15:39:59 +0200

    asdas

--- Proyectos Nahuel/Tema Contenedores/src/fruta/BotigaDeFruites.java
+++ Proyectos Nahuel/Tema Contenedores/src/fruta/BotigaDeFruites.java
@@ -1,18 +1,31 @@
 package fruta;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class BotigaDeFruites {
     ArrayList<Fruit> frutas;
 
     void afegirFruit(Fruit fruit){
-
+        frutas.add(fruit);
+        System.out.println("Se ha añadido correctamente");
     }
 
     void eliminarFruit(String nom){
+        
+        Iterator<Fruit> fruta= frutas.iterator();
+        if(fruta.equals(nom)){
+            
+        }
+
+        
+        
 
     }
     double getPreuTotal(){
+        
         return 0; 
     }
+
+    
 }
--- Proyectos Nahuel/Tema Contenedores/src/fruta/Fruit.java
+++ Proyectos Nahuel/Tema Contenedores/src/fruta/Fruit.java
@@ -3,5 +3,8 @@ package fruta;
 public class Fruit {
     String nombre;
     double precio;
-    
+
+    public String toString(){
+        return "Nombre: " + this.nombre + "Precio: " + this.precio;
+    }
 }
