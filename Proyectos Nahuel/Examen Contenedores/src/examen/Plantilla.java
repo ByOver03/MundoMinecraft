7e97656cbde9e179bb637355fd3d204dc28445b9
ByOver03 <nahuelrobert1718@gmail.com>
2024-04-29 20:03:32 +0200

    nuevas cosas

--- Proyectos Nahuel/Examen Contenedores/src/examen/Plantilla.java
+++ Proyectos Nahuel/Examen Contenedores/src/examen/Plantilla.java
@@ -6,6 +6,7 @@ import java.examen.MetodosAuxiliares;
 
 public class Plantilla {
     
+    //Metodo para dar de alta a un jugaor
     static void altaJugador(Map<Integer, Jugador> plantilla, Integer dorsal){
         MetodosAuxiliares m = new MetodosAuxiliares();
         Scanner leer = new Scanner(System.in);
@@ -18,19 +19,23 @@ public class Plantilla {
         plantilla.put(dorsal, j);
     }
 
+    //Metodo para dar de baja a un jugador segun el dorsal introducido
     static Jugador eliminarJugador(Map<Integer, Jugador> plantilla, Integer dorsal){
 
         return null;
     }
 
+    //Metodo parea mostrar a los jugadores segun la posicion Introducida por el usuario
     static void mostrar(Map<Integer, Jugador>plantilla, String posicion){
 
     }
 
+    //Metodo parea mostrar a los jugadores
     static void mostrar(Map<Integer, Jugador> plantilla){
 
     }
 
+    //Metodo para editar los datos de un jugador
     static boolean editarJugador(Map<Integer, Jugador> plantilla, Integer dorsal){
         return false;
     }
