7e94c5fc3ecad1bbd368bc3865da78a7668224c2
ByOver03 <nahuelrobert1718@gmail.com>
2024-05-13 15:41:12 +0200

    Modificaciones Librerias

--- Proyectos Nahuel/LibreriasJava/src/scanner/LAsdas.java
+++ Proyectos Nahuel/LibreriasJava/src/scanner/LAsdas.java
@@ -6,13 +6,23 @@ public class LAsdas {
 
 	public static int ScannerInteger(int integer) {
 		Scanner leer = new Scanner(System.in);
-		return integer = leer.nextInt();
+		integer = leer.nextInt();
+		leer.close();
+		return integer;
 	}
 	
 
 	public static String ScannerString (String string) {
 		Scanner leer = new Scanner(System.in);
-		return string = leer.next();
+		string = leer.next();
+		leer.close();
+		return string;
+	}
+	public static double Scannerdouble(double doble){
+		Scanner leer = new Scanner(System.in);
+		doble = leer.nextDouble();
+		leer.close();
+		return doble;
 	}
 
 }
