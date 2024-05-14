f77a5538093420041a7493348530e3459c3b3e7f
ByOver03 <nahuelrobert1718@gmail.com>
2024-05-14 11:37:31 +0200

    Escribe FIchero Binario

--- Proyectos Nahuel/LibreriasJava/src/gestionFicheros/EscrbirFicherosBinarios.java
+++ Proyectos Nahuel/LibreriasJava/src/gestionFicheros/EscrbirFicherosBinarios.java
@@ -1,12 +1,43 @@
 package gestionFicheros;
 
-import java.io.*;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class EscrbirFicherosBinarios {
     
-    public static void escribeFicheroStringToObject(String cadena, String fichero) {    
+    public static <T> void escribeFicheroGenericoToObject(T cadena, String fichero) {    
         try(ObjectOutputStream ous = new ObjectOutputStream(new FileOutputStream(fichero))) {
             ous.writeObject(cadena);
+
+        } catch (IOException e) {
+            System.out.println("Error de escritura de fichero: " + e.getMessage());
+        }
+    }
+
+    public static <T> void escribeFicheroListGenericoAObject(List<T> lista, String fichero){
+        try(ObjectOutputStream ous = new ObjectOutputStream(new FileOutputStream(fichero))) {
+            ous.writeObject(lista);
+
+        } catch (IOException e) {
+            System.out.println("Error de escritura de fichero: " + e.getMessage());
+        }
+    }
+    public static <T> void escribeFicheroSetBinarioAObject(Set<T> set, String fichero){
+        try(ObjectOutputStream ous = new ObjectOutputStream(new FileOutputStream(fichero))) {
+            ous.writeObject(set);
+
+        } catch (IOException e) {
+            System.out.println("Error de escritura de fichero: " + e.getMessage());
+        }
+    }
+
+    public static <T,V> void escribeFicheroMapaGenericoAObject(Map<T,V> mapa, String fichero){
+        try(ObjectOutputStream ous = new ObjectOutputStream(new FileOutputStream(fichero))) {
+            ous.writeObject(mapa);
 
         } catch (IOException e) {
             System.out.println("Error de escritura de fichero: " + e.getMessage());
