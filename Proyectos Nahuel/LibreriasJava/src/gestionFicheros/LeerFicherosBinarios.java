3d0684d6aa078113b0edbcf0874fcc791211e81c
ByOver03 <nahuelrobert1718@gmail.com>
2024-05-14 12:54:52 +0200

    Libreria ficheros binarios terminada

---
+++ Proyectos Nahuel/LibreriasJava/src/gestionFicheros/LeerFicherosBinarios.java
@@ -0,0 +1,63 @@
+package gestionFicheros;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class LeerFicherosBinarios {
+    public static <T>T leerFicheroObjectToGenerico(String fichero) {
+        T cadena = null;
+        try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fichero))) {
+            cadena = (T)ois.readObject();
+
+        } catch (IOException e) {
+            System.out.println("Error de lectura de fichero: ");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error de conversión de datos: " + e.getMessage());
+        }
+        return cadena;
+    }
+
+    public static <T>List<T> leerFicheroObjetoAListaGenerica(String fichero){
+        List<T> lista= null;
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fichero))){
+            lista = (List)ois.readObject();
+        } catch (IOException e) {
+            System.out.println("Error de lectura de fichero: ");
+            e.printStackTrace();
+        }catch(ClassNotFoundException e){
+            System.out.println("Error de conversion de datos: " + e.getMessage());
+        }
+        return lista;
+    }
+
+    public static<T> Set<T> leerFicheroObjetoASetGenerico(String fichero){
+        Set<T> set = null;
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fichero))){
+            set = (Set)ois.readObject();
+        } catch (IOException e) {
+            System.out.println("Error de lectura de fichero: ");
+            e.printStackTrace();
+        }catch(ClassNotFoundException e){
+            System.out.println("Error de conversion de datos: " + e.getMessage());
+        }
+        return set;
+    }
+
+    public static <T,V> Map<T,V> leerFicheroObjetoAMapaGenerico(String fichero){
+        Map<T,V> mapa = null;
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fichero))){
+            mapa = (Map)ois.readObject();
+        } catch (IOException e) {
+            System.out.println("Error de lectura de fichero: ");
+            e.printStackTrace();
+        }catch(ClassNotFoundException e){
+            System.out.println("Error de conversion de datos: " + e.getMessage());
+        }
+        return mapa;
+    }
+}
